09468981c1203e7339a87e35b999ac1714103a06
JaredJose <jjose@ucsd.edu>
2022-02-03 12:12:53 -0800

    Updated MarkdownParse.java to address new failure inducing input

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,8 +15,9 @@ public class MarkdownParse {
 
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
+            int closeParen = markdown.indexOf(")", openParen);
 
-            if(nextCloseBracket == -1 || openParen == -1 || nextOpenBracket == -1){
+            if(nextCloseBracket == -1 || openParen == -1 || nextOpenBracket == -1 || closeParen == -1){
                 break;
             }
 
@@ -25,7 +26,6 @@ public class MarkdownParse {
                 continue;
             }
 
-            int closeParen = markdown.indexOf(")", openParen);
 
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
